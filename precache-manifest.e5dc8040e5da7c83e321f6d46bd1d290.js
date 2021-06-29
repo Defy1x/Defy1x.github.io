b9ab7385225756cb035b29b36a447a8a7477acac
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-06-28 23:54:58 -0400

    Updates

---
+++ precache-manifest.e5dc8040e5da7c83e321f6d46bd1d290.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2f04cfedef57c90d11d39e16442c94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b45382da761feff2f2f",
+    "url": "/static/css/2.dbc07da6.chunk.css"
+  },
+  {
+    "revision": "d1de61125e13f6af8d5d",
+    "url": "/static/css/main.cb0789d6.chunk.css"
+  },
+  {
+    "revision": "8b45382da761feff2f2f",
+    "url": "/static/js/2.37557166.chunk.js"
+  },
+  {
+    "revision": "d1de61125e13f6af8d5d",
+    "url": "/static/js/main.8ec31181.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
